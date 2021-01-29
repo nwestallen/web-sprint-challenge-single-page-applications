a056ebdcb1cdb337a3b6ea5e89030873b04dba74
Nick Allen <nwestallen@gmail.com>
2021-01-29 11:15:38 -0500

    added name validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 //Form component
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import * as Yup from 'yup';
 
 const StyledForm = styled.form`
     display: flex;
@@ -8,14 +9,49 @@ const StyledForm = styled.form`
     align-items: left;
     border: 3px solid black;
     width: 300px;
+
+    .error {
+        color: red;
+        font-size: 10px;
+    }
 `;
+
+const formSchema = Yup.object().shape({
+    name: Yup
+    .string()
+    .min(2, 'Must be at least 2 characters long'),
+    size: Yup
+    .string(),
+    pepperoni: Yup
+    .boolean(),
+    sausage: Yup
+    .boolean(),
+    mushroom: Yup
+    .boolean(),
+    greenPepper: Yup
+    .boolean(),
+    onion: Yup
+    .boolean(),
+    notes: Yup
+    .string()
+})
 
 const Form = props => {
 
     const[orderItem, setOrderItem] = useState({name:'',size:'',pepperoni:false,sausage:false,mushroom:false,greenPepper:false,onion:false,notes:''});
+    const[errors, setErrors] = useState({
+        name:''
+    })
 
     const handleChange = event => {
         const { name, type, value, checked } = event.target
+
+        Yup
+        .reach(formSchema, name)
+        .validate(value)
+        .then(valid => setErrors({...errors, [name]: ''}))
+        .catch(err => setErrors({...errors, [name]: err.errors[0]}));
+
         if(type === 'checkbox'){
             setOrderItem({...orderItem, [name]: checked})
         } else {
@@ -28,6 +64,7 @@ const Form = props => {
             <h2>Pls order pizza</h2>
             <label>Name: 
                 <input type='text' name='name' value={orderItem.name} onChange={handleChange} />
+                {errors.name.length > 0 && <p className='error'>{errors.name}</p>}
             </label>
             <label>Size:
                 <select name='size' value={orderItem.size} onChange={handleChange}>

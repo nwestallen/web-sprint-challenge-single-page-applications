855b197190f2f85c65cf59511dfec39fcc80cf7b
Nick Allen <nwestallen@gmail.com>
2021-01-29 08:36:18 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <div className='App'>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </>
+    </div>
   );
 };
 export default App;
